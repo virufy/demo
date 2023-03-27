f7f141f28c14cc68546ffaecb6111fba1676e367
ferlrodriguez <fernando.rodriguez@xoor.io>
2023-03-27 15:52:52 -0300

    fix(): pr feedback VIR-398

--- src/components/WizardButtons/style.ts
+++ src/components/WizardButtons/style.ts
@@ -1,8 +1,5 @@
 import styled from 'styled-components';
 
-// Images
-import { ReactComponent as NuevaSVG } from 'assets/images/nueva.svg';
-
 export const WizardButtonsContainer = styled.div`
   display: flex;
   align-items: center;
@@ -25,9 +22,4 @@ export const WizardButtonsContainer = styled.div`
   @media screen and (${props => props.theme.breakpoints.tablet}){
     max-width: 470px;
   }
-`;
-
-export const NuevaLogo = styled(NuevaSVG)`
-  position: absolute;
-  left: 36px;
 `;
