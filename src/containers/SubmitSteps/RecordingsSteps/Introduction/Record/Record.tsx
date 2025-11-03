fded7cf8522c94da32f4ca33d2f3487d50baff01
elsy0111 <kpp0193@gmail.com>
2025-11-03 23:07:45 +0900

    fix: delete minimum recording time

--- src/containers/SubmitSteps/RecordingsSteps/Introduction/Record/Record.tsx
+++ src/containers/SubmitSteps/RecordingsSteps/Introduction/Record/Record.tsx
@@ -29,28 +29,6 @@ const schema = Yup.object({
         return (size <= 1024 ** 3 * audioMaxSizeInMb);
       }
       return !!value;
-    })
-    .test('fileDuration', 'ERROR.FILE_DURATION', async (value?: any) => {
-      if (value) {
-        const file = value as File;
-        const audio = new Audio(URL.createObjectURL(file));
-        audio.load();
-        await new Promise(resolver => audio.addEventListener('loadedmetadata', resolver));
-        const duration: number = await new Promise(resolver => {
-          if (audio.duration !== Infinity) {
-            resolver(audio.duration);
-          }
-          audio.addEventListener('durationchange', () => {
-            audio.remove();
-            resolver(audio.duration);
-          });
-          audio.volume = 0;
-          audio.currentTime = 24 * 60 * 60; // Unprobable time
-          audio.play();
-        });
-        return (duration >= 2);
-      }
-      return !!value;
     }),
 }).defined();
 
